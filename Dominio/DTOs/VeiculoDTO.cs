adb82f0290e6ebf8d12e1529625a254726e69103
FabioYoko <fabiopython.br@gmail.com>
2024-08-05 11:50:20 -0300

    Criando os metodos, criar, listar, atualizar e deletar

---
+++ Dominio/DTOs/VeiculoDTO.cs
@@ -0,0 +1,9 @@
+
+namespace MiminalApi.DTOs;
+public record VeiculoDTO 
+{
+  
+      public string Nome { get;set; } = default!;
+     public string Marca { get;set; } = default!;
+    public int Ano { get;set; } = default!;
+};
