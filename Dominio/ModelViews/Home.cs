c21f9469765a203aa06463fb773a4f96692dfb4e
FabioYoko <fabiopython.br@gmail.com>
2024-08-05 09:20:19 -0300

    "Criação da tabela veiculo"

---
+++ Dominio/ModelViews/Home.cs
@@ -0,0 +1,7 @@
+namespace MiminalApi.Dominio.ModelViews;
+
+public struct Home 
+{
+    public string Mensagem { get => "Bem vindo a API de veículos - Miminal API"; }
+    public string Doc { get => "/swagger"; }
+}
--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using MiminalApi.Dominio.Entidades;
 using MiminalApi.Dominio.Interfaces;
+using MiminalApi.Dominio.ModelViews;
 using MiminalApi.Dominio.Servicos;
 using MiminalApi.DTOs;
 using MiminalApi.Infraestrutura.Db;
@@ -9,6 +10,9 @@ using MiminalApi.Infraestrutura.Db;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddScoped<IAdministradorServico, AdministradorServico>();
+
+builder.Services.AddEndpointsApiExplorer();
+object value = builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<DbContexto>(options => {
     options.UseMySql(
@@ -20,16 +24,18 @@ builder.Services.AddDbContext<DbContexto>(options => {
 var app = builder.Build();
 
 
-app.MapGet("/", () => "Olá pessoal!");
+app.MapGet("/", () => Results.Json(new Home()));
 
 app.MapPost("/login", ([FromBody] LoginDTO loginDTO, IAdministradorServico administradorServico) => {
     if(administradorServico.Login(loginDTO) != null){
-        return Results.Ok("Logincom sucesso");
+        return Results.Ok("Login com sucesso");
     } else {
         return Results.Unauthorized();
     }
 });
 
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.Run();
 
